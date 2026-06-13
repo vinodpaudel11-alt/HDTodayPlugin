e197306f264e3ee6bcc3c446de6e9dc91e34fb7b
vinodpaudel11-alt <vinodpaudel11@gmail.com>
2026-06-13 11:35:02 +0545

    Add files via upload

--- HDTodayProvider/build.gradle.kts
+++ HDTodayProvider/build.gradle.kts
@@ -1,1 +1,28 @@
+// use an integer for version numbers
+version = 1
 
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    description = "HDToday / GoTVSeries – TV Shows using TMDB IDs and VidSrc embeds"
+    authors = listOf("CloudStream")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Currently only TMDB is supported. See
+    // https://recloudstream.github.io/csdocs/library/
+    tvTypes = listOf(
+        "TvSeries",
+        "Anime",
+    )
+
+    iconUrl = "https://gotvseries.top/images/favicon.png"
+}
